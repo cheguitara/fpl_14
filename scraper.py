7fc201ab953c0436f161e2d0398eccf885f259c8
cheguitara <cardactiveunit@gmail.com>
2014-03-22 10:49:00 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -208,7 +208,7 @@ def import_player(element):
 #        table_name='fixtures')
 
 def main():#663
-    for element in elements(1, 5):
+    for element in elements(1, 665):
         import_player(element)
         #import_season_history(element)
         import_fixture_history(element)
